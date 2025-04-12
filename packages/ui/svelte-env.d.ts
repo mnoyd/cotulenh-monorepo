1d6050d3b1ceb6bdef25ef18f6f3234a1d9f4914
Hoang Manh <mnoy.dev@gmail.com>
2025-04-13 02:22:08 +0700

    chore: remove generated code in sample

--- packages/ui/svelte-env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="svelte" />
-/// <reference types="vite/client" /> // Assuming Vite, remove if not applicable
-
-// Declare Svelte components as modules
-declare module '*.svelte' {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	export default class extends SvelteComponent<any, any, any> {}
-}
