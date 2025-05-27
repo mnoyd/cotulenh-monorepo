fd8649210747496368b800e77817f7092af49ca9
Hoang Manh <noydev@gmail.com>
2025-05-27 14:40:57 +0700

    refactor: Remove cotulenh-notation as we don't translate between coordinate anymore.

--- apps/cotulenh-app/vite.config.ts
+++ apps/cotulenh-app/vite.config.ts
@@ -2,11 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	optimizeDeps: {
-		include: ['@repo/cotulenh-notation']
-	},
-	ssr: {
-		noExternal: ['@repo/cotulenh-notation']
-	}
+  plugins: [sveltekit()],
+  optimizeDeps: {
+    include: []
+  },
+  ssr: {
+    noExternal: []
+  }
 });
--- packages/cotulenh-notation/vitest.config.js
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vitest/config';
-import { createVitestConfig } from '@repo/vitest-config/vitest.setup.js';
-
-export default defineConfig(createVitestConfig({
-  // Add any package-specific configuration here
-}));
