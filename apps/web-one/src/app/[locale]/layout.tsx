935cfc62fe7fa147ef34488bfb65d2f9bcfdc4f4
Duong <iamtdthewinner@gmail.com>
2025-08-04 17:18:23 +0700

    add next-intl

---
+++ apps/web-one/src/app/[locale]/layout.tsx
@@ -0,0 +1,57 @@
+import { Geist, Geist_Mono } from 'next/font/google';
+import '../globals.css';
+import { ThemeProvider } from '@/Providers/ThemeProvider';
+import { Locale, NextIntlClientProvider, hasLocale } from 'next-intl';
+import { notFound } from 'next/navigation';
+import { routing } from '@/i18n/routing';
+import { getTranslations } from 'next-intl/server';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin']
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin']
+});
+
+export async function generateMetadata({ params }: { params: Promise<{ locale: Locale }> }) {
+  const { locale } = await params;
+  const t = await getTranslations({ locale });
+
+  return {
+    title: t('main.title')
+  };
+}
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
+
+export default async function RootLayout({
+  children,
+  params
+}: Readonly<{
+  children: React.ReactNode;
+  params: Promise<{ locale: string }>;
+}>) {
+  const { locale } = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  return (
+    <html lang={locale} suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <NextIntlClientProvider>{children}</NextIntlClientProvider>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
