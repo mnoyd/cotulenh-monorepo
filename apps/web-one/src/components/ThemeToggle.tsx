8a23ffb883776a6e49bc0122f252ae0355d34be8
Duong <iamtdthewinner@gmail.com>
2025-08-04 16:34:04 +0700

    add dark mode and theme toggle

---
+++ apps/web-one/src/components/ThemeToggle.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { Moon, Sun } from 'lucide-react';
+import { useTheme } from 'next-themes';
+import * as React from 'react';
+
+import { Button } from '@/components/ui/button';
+
+export default function ThemeToggle() {
+  const { setTheme, resolvedTheme } = useTheme();
+
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <Button
+      variant="outline"
+      size="icon"
+      onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
+    >
+      {resolvedTheme === 'dark' ? (
+        <Sun className="h-[1.2rem] w-[1.2rem] transition-all" />
+      ) : (
+        <Moon className="h-[1.2rem] w-[1.2rem] transition-all" />
+      )}
+    </Button>
+  );
+}
